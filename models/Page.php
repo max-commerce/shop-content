0792480cc1866674f633c0aefbb34cb3013753d0
web-mayak <web-mayak@mail.ru>
2017-10-07 17:31:03 +1000

    maxcommerce v.2 implementation

--- models/Page.php
+++ models/Page.php
@@ -8,10 +8,28 @@ class Page extends \yii\db\ActiveRecord
 {
     public static function tableName()
     {
-        return 'shop_page';
+
+    	if (Yii::$app->db->schema->getTableSchema('shop_page') !== null) {
+    		// maxcommerce v.1 implementation
+        	return 'shop_page';
+    	} elseif (Yii::$app->db->schema->getTableSchema('pages') !== null) {
+    		// maxcommerce v.2 implementation
+			return 'pages';
+    	} else {
+    		throw new ErrorException("Can't find `pages` table");
+    	}
     }
 
     public function getUrl(){
         return Url::to(['/' . $this->alias]);
     }
+
+    public function getText(){
+
+		// maxcommerce v.2 implementation
+    	if ($this->hasAttribute('content')) {
+        	return $this->content;
+        }
+    }
+
 }
